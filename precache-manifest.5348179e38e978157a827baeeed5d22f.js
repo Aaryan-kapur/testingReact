ea35e144cb5296d0800f5135f3046249699b9414
aaryan-kapur <ak6392@bennett.edu.in>
2020-05-08 07:30:34 +0530

    Updates

---
+++ precache-manifest.5348179e38e978157a827baeeed5d22f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7874ef4dd177c531f7da623b521eaf9c",
+    "url": "/testingReact/index.html"
+  },
+  {
+    "revision": "d9f3e701977ea827449f",
+    "url": "/testingReact/static/css/main.0bdaf6ea.chunk.css"
+  },
+  {
+    "revision": "65ea2370c5d8dedaa06a",
+    "url": "/testingReact/static/js/2.39f0647d.chunk.js"
+  },
+  {
+    "revision": "30c40d404319c6bcbaa4f69d872fdf2e",
+    "url": "/testingReact/static/js/2.39f0647d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f3e701977ea827449f",
+    "url": "/testingReact/static/js/main.b9484c7a.chunk.js"
+  },
+  {
+    "revision": "bdd59f6715d611c580e6",
+    "url": "/testingReact/static/js/runtime-main.3d64cbd8.js"
+  }
+]);
